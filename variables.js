aa1cb7d241659c69d192c8a012e3d25b2da94b8a
nafkad <akadri.work@gmail.com>
2023-12-01 17:05:33 -0500

    Started Skills

---
+++ variables.js
@@ -0,0 +1,43 @@
+//////////////////PROBLEM 1////////////////////
+/* Create a variable called 'myName' with a value that is your name as a string. */
+
+//CODE HERE 
+const myName = "Adenike";
+console.log(myName)
+
+
+//////////////////PROBLEM 2////////////////////
+/* Create a variable called 'faveNum' with a value that is your favorite number. */
+
+//CODE HERE
+const faveNum = 100;
+console.log(faveNum);
+
+//////////////////PROBLEM 3////////////////////
+/* Create a variable called 'lovesCode' and assign it a boolean value. */
+
+//CODE HERE
+const lovesCode = ???
+console.log(lovesCode);
+
+//////////////////PROBLEM 4////////////////////
+/*
+  Write an if-statement that checks if the faveNum variable is 
+  equal to 13. If it is, console.log('lucky 13!'). 
+  If it's not, console.log('not lucky 13').
+*/
+
+//CODE HERE
+const faveNum = ???
+if ???
+console.log(faveNum); // ???
+
+//////////////////PROBLEM 5////////////////////
+/*
+  Write a for loop that prints out the numbers from 1 through 5.
+*/
+
+//CODE HERE
+let ???
+???
+console.log(???); // ???
